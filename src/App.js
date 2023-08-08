14249d6f616b2df3c99bc4cdd8803cb03e6e36df
Shama Parveen <85455446+Shama-Coder7@users.noreply.github.com>
2023-08-09 01:18:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Foods from './Components/Foods';
+
+function App() {
+  return (
+    <div className="container">
+      <Foods/>
+    </div>
+  );
+}
+
+export default App;
